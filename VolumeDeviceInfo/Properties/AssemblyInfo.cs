a438c3308ebf1df8d65b72598cb7a764dbbc1138
Jason Curl <jcurl@arcor.de>
2023-06-25 19:41:36 +0200

    VolumeDeviceInfo: Factor out net40 testing as a new project
    Since Microsoft dropped support for testing .NET 4.0 projects in Microsoft.NET.Test.Sdk 17.4.0 and later, the project must be refactored that a test project is .NET 4.8 and explicitly uses the .NET 4.0 library to test.
    
    The two projects are in the same folder, so the new project must be configured to build in a newer folder, else build will fail as previous artifacts from another build using the same folder will interfere (e.g. `obj/project.assets.json`).
    
    Two projects, one specifically handling legacy, gives flexibility later when modifying the current project.
    
    Issue: DOTNET-827

--- VolumeDeviceInfo/Properties/AssemblyInfo.cs
+++ VolumeDeviceInfo/Properties/AssemblyInfo.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
 [assembly: CLSCompliant(true)]
-
-#if !SIGN
-// Required for Unit Testing and Logging
-[assembly: InternalsVisibleTo("RJCP.IO.VolumeDeviceInfoTest")]
-[assembly: InternalsVisibleTo("VolumeInfo")]
-#endif
